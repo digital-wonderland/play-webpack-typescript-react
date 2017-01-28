c3f12ec70a8f9a943f60fa59dd1dd6dd185f56aa
Stephan Kleine <stephan.kleine@gmail.com>
2017-01-28 21:21:23 +0700

    add 'use strict'

--- jestPreProcessor.js
+++ jestPreProcessor.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const tsc = require('typescript')
 const babelJest = require('babel-jest')
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var webpack = require('webpack')
 var path = require('path')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
