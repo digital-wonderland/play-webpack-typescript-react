64c5de7d45fdba3e35ced6438f6a5d951853cf45
Stephan Kleine <stephan.kleine@gmail.com>
2017-02-15 00:36:34 +0700

    make application.conf overridable in Dev mode

---
+++ src/main/scala/MyGuiceApplicationLoader.scala
@@ -0,0 +1,18 @@
+import com.typesafe.config.{Config, ConfigFactory}
+import play.api.inject.guice.{GuiceApplicationBuilder, GuiceApplicationLoader}
+import play.api.{ApplicationLoader, Configuration, Mode}
+
+class MyGuiceApplicationLoader extends GuiceApplicationLoader() {
+
+  override def builder(context: ApplicationLoader.Context): GuiceApplicationBuilder = {
+    val initialConfig = context.initialConfiguration.underlying
+
+    val config: Config = context.environment.mode match {
+      case Mode.Dev => ConfigFactory.load("application-dev.conf").withFallback(initialConfig)
+      case _ => initialConfig
+    }
+
+    super.builder(context).configure(new Configuration(config))
+  }
+
+}
